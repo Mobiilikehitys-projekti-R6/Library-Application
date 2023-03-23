9c2667da0b4ebe70e26be11b204dacda9c8e4e35
temonn <t1poti00@students.oamk.fi>
2023-03-23 13:32:29 +0200

    navigationbar toimii?

---
+++ library_app/lib/pages/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      backgroundColor: Colors.transparent,
+    ),
+    body: Container(
+      alignment: Alignment.center,
+    ),
+  ); 
+}
