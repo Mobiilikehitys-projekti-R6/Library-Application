dd27de463691d4761dcf5ec386b3426a1cc17231
temonn <t1poti00@students.oamk.fi>
2023-04-10 12:21:09 +0300

    profileSet UI parannettu + turhat koodit poistettu

--- library_app/lib/pages/profile.dart
+++ library_app/lib/pages/profile.dart
@@ -1,4 +1,7 @@
-/*import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:library_app/pages/login_page.dart';
 import 'package:library_app/pages/profileSet.dart';
 
@@ -10,12 +13,33 @@ class Myprofile extends StatefulWidget {
 }
 
 class _MyprofileState extends State<Myprofile> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  String _name = '';
+  String _email = '';
+  String _info = '';
+  File? _image;
 
-   @override
+  void _updateProfile(String name, String email, String info) {
+    setState(() {
+      _name = name;
+      _email = email;
+      _info = info;
+    });
+  }
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Color(0xFF25BE70),
@@ -25,7 +49,13 @@ class _MyprofileState extends State<Myprofile> {
           children: <Widget>[
             IconButton(
               onPressed: () {
-                _scaffoldKey.currentState!.openDrawer();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        ProfileSettings(updateProfile: _updateProfile),
+                  ),
+                );
               },
               icon: Icon(Icons.settings),
             ),
@@ -50,264 +80,50 @@ class _MyprofileState extends State<Myprofile> {
         ),
         centerTitle: true,
       ),
-      drawer: Drawer(
-        child: ListView(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ListTile(
-              title: Text("Muokkaa profiilia"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ProfileSettings()),
-                );
-              },
+            InkWell(
+              onTap: _pickImage,
+              child: CircleAvatar(
+              backgroundImage: _image != null
+                ? FileImage(_image!)
+                : AssetImage('') as ImageProvider<Object>?,
+              radius: 70,
+              ),
             ),
-            ListTile(
-              title: Text("Poista tilisi"),
-              onTap: () {
-              },
+            SizedBox(height: 10),
+            Text(
+              _name,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            ListTile(
-              title: Text("Tietoa meistä"),
-              onTap: () {
-              },
+            SizedBox(height: 5),
+            Text(
+              _email,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[600],
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                _info,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[700],
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
           ],
-        ),
-      ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('images/profile-pic.jpg'),
-                radius: 70,
-              ),
-              SizedBox(height: 10),
-              Text(
-                'erkki',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 5),
-              Text(
-                'Opiskelija',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey[600],
-                ),
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel faucibus quam. Nullam at turpis convallis, luctus metus eu, mattis est. Sed ultrices ac ipsum non placerat. Donec euismod lacus eget elit blandit, vel bibendum sapien pellentesque. Vestibulum a blandit mi.',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey[700],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
-  }
-}*/
-/*
-import 'package:flutter/material.dart';
-import 'package:library_app/pages/login_page.dart';
-import 'package:library_app/pages/profileSet.dart';
-
-class Myprofile extends StatefulWidget {
-  const Myprofile({Key? key}) : super(key: key);
-
-  @override
-  State<Myprofile> createState() => _MyprofileState();
-}
-
-
-class _MyprofileState extends State<Myprofile> {
-  String _name = '';
-  String _email = '';
-  String _info = '';
-
-
-  void _updateProfile(String name, String email, String info) {
-    setState(() {
-      _name = name;
-      _email = email;
-      _info = info;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profiili'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProfileSettings(updateProfile: _updateProfile)),
-              );
-            },
-            icon: Icon(Icons.edit),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Nimi',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(_name),
-              SizedBox(height: 20),
-              Text(
-                'Sähköposti',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(_email),
-              SizedBox(height: 20),
-              Text(
-                'Info',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(_info),
-            ],
-          ),
         ),
       ),
     );
-  }
-}*/
-
-import 'package:flutter/material.dart';
-import 'package:library_app/pages/login_page.dart';
-import 'package:library_app/pages/profileSet.dart';
-
-class Myprofile extends StatefulWidget {
-  const Myprofile({Key? key}) : super(key: key);
-
-  @override
-  State<Myprofile> createState() => _MyprofileState();
-}
-
-class _MyprofileState extends State<Myprofile> {
-  String _name = '';
-  String _email = '';
-  String _info = '';
-
-
-  void _updateProfile(String name, String email, String info) {
-    setState(() {
-      _name = name;
-      _email = email;
-      _info = info;
-    });
-  }
-
-   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Color(0xFF25BE70),
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProfileSettings(updateProfile: _updateProfile)),
-              );
-              },
-              icon: Icon(Icons.settings),
-            ),
-            Text(
-              'Profiili',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 27,
-              ),
-            ),
-            IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginPage()),
-                );
-              },
-              icon: Icon(Icons.logout),
-            ),
-          ],
-        ),
-        centerTitle: true,
-      ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('images/profile-pic.jpg'),
-                radius: 70,
-              ),
-              SizedBox(height: 10),
-              Text(
-                _name,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 5),
-              Text(
-                _email,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey[600],
-                ),
-              ),
-              SizedBox(height: 20),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Text(
-                  _info,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey[700],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
   }
 }
--- library_app/lib/pages/profileSet.dart
+++ library_app/lib/pages/profileSet.dart
@@ -1,105 +1,3 @@
-/*import 'package:flutter/material.dart';
-
-class ProfileSettings extends StatefulWidget {
-  const ProfileSettings({Key? key}) : super(key: key);
-
-  @override
-  _ProfileSettingsState createState() => _ProfileSettingsState();
-}
-
-class _ProfileSettingsState extends State<ProfileSettings> {
-  final _formKey = GlobalKey<FormState>();
-
-  String _name = '';
-  String _email = '';
-  String _info = '';
-
-  TextEditingController _nameController = TextEditingController();
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _infoController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _nameController.text = _name;
-    _emailController.text = _email;
-    _infoController.text = _info;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Muokkaa profiilia'),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  labelText: 'Nimi',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Nimi ei voi olla tyhjä';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                  labelText: 'Sähköposti',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Sähköposti ei voi olla tyhjä';
-                  }
-                  if (!value.contains('@')) {
-                    return 'Sähköpostiosoite ei ole kelvollinen';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _infoController,
-                decoration: InputDecoration(
-                  labelText: 'Info',
-                ),
-                maxLines: null,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Info ei voi olla tyhjä';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    setState(() {
-                      _name = _nameController.text;
-                      _email = _emailController.text;
-                      _info = _infoController.text;
-                    });
-                    //Navigator.pop(context);
-                  }
-                },
-                child: Text('Tallenna'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}*/
-
 import 'package:flutter/material.dart';
 
 class ProfileSettings extends StatefulWidget {
@@ -134,7 +32,18 @@ class _ProfileSettingsState extends State<ProfileSettings> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Muokkaa profiilia'),
+        automaticallyImplyLeading: false,
+        backgroundColor: Color(0xFF25BE70),
+        elevation: 0,
+        title: const Text(
+          'Asetukset',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 27,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: Form(
         key: _formKey,
@@ -183,19 +92,36 @@ class _ProfileSettingsState extends State<ProfileSettings> {
                 },
               ),
               SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    setState(() {
-                      _name = _nameController.text;
-                      _email = _emailController.text;
-                      _info = _infoController.text;
-                    });
-                    widget.updateProfile(_name, _email, _info);
-                    //Navigator.pop(context);
-                  }
-                },
-                child: Text('Tallenna'),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFF25BE70),
+                  border: Border.all(color: Colors.white),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.grey,
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      setState(() {
+                        _name = _nameController.text;
+                        _email = _emailController.text;
+                        _info = _infoController.text;
+                      });
+                      widget.updateProfile(_name, _email, _info);
+                      //Navigator.pop(context);
+                    }
+                  },
+                  child: const Text(
+                    'Tallenna',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
             ],
           ),
@@ -204,3 +130,4 @@ class _ProfileSettingsState extends State<ProfileSettings> {
     );
   }
 }
+
