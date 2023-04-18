ed6126f7733baaf41c910edde51b567842f172e6
temonn <t1poti00@students.oamk.fi>
2023-04-18 16:44:48 +0300

    Adminpagesta voi ladata kuvia Firebase:en

--- library_app/lib/pages/addBooks.dart
+++ library_app/lib/pages/addBooks.dart
@@ -1,22 +1,109 @@
+import 'dart:io';
+import 'package:uuid/uuid.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class AdminPage extends StatelessWidget {
+class AdminPage extends StatefulWidget {
+  @override
+  _AdminState createState() => _AdminState();
+}
+
+class _AdminState extends State<AdminPage> {
+  final _formKey = GlobalKey<FormState>();
+  File _imageFile = File('');
+  String _name = '';
+  String _reservationDate = '';
+
+  Future<void> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+    setState(() {
+      if (pickedFile != null) {
+        _imageFile = File(pickedFile.path);
+      }
+    });
+  }
+  
+Future<void> _uploadImageToFirebase() async {
+  if (_imageFile == null) {
+    return;
+  }
+
+  final uuid = Uuid();
+  final uniqueId = uuid.v4(); // generates a random UUID
+
+  final Reference firebaseStorageRef =
+      FirebaseStorage.instance.ref().child('profile-images/$uniqueId.jpg');
+
+  final metadata = SettableMetadata(
+      contentType: 'image/jpeg',
+      customMetadata: {
+        'name': _name,
+      });
+
+  final UploadTask uploadTask =
+      firebaseStorageRef.putFile(_imageFile, metadata);
+
+  final TaskSnapshot downloadUrl = (await uploadTask);
+  final String url = await downloadUrl.ref.getDownloadURL();
+
+  print("URL of the uploaded image: $url");
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xFF25BE70),
         title: Text('Admin Page'),
       ),
       body: Center(
-        child: Text(
-          'Welcome, Admin!',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 20.0),
+              _imageFile == null
+                  ? Text('No image selected.')
+                  : Image.file(_imageFile),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text('Choose Image'),
+                onPressed: () {
+                  _pickImage(ImageSource.gallery);
+                },
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Enter name',
+                ),
+                onChanged: (value) {
+                  _name = value;
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                child: Text('Upload Image'),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _uploadImageToFirebase();
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
--- library_app/lib/pages/home.dart
+++ library_app/lib/pages/home.dart
@@ -8,186 +8,220 @@ class MyHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Color.fromARGB(68, 255, 255, 255),
-          elevation: 0,
-          title: const Text(
-            'Aloitussivu',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 27,
-            ),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Color.fromARGB(68, 255, 255, 255),
+        elevation: 0,
+        title: const Text(
+          'Aloitussivu',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 27,
           ),
-          centerTitle: true,
-          actions: [
-IconButton(
-  icon: Icon(Icons.admin_panel_settings),
-  color: Color(0xFF25BE70),
-  onPressed: () async {
-final snapshot = await FirebaseFirestore.instance
-    .collection("users")
-    .where("isAdmin", isEqualTo: true)
-    .get();
-
-if (snapshot.docs.isNotEmpty) {
-  Navigator.push(
-    context,
-    MaterialPageRoute(
-      builder: (context) => AdminPage(),
-    ),
-  );
-} else {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text("Tapahtui virhe!"),
-        content: Text("Tämä sivu on vain Järjestelmävalvojille!"),
+        ),
+        centerTitle: true,
         actions: [
-          TextButton(
-            child: Text("OK"),
-            onPressed: () => Navigator.pop(context),
+          IconButton(
+            icon: Icon(Icons.admin_panel_settings),
+            color: Color(0xFF25BE70),
+            onPressed: () async {
+              final snapshot = await FirebaseFirestore.instance
+                  .collection("users")
+                  .where("isAdmin", isEqualTo: true)
+                  .get();
+              if (snapshot.docs.isNotEmpty) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AdminPage(),
+                  ),
+                );
+              } else {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Tapahtui virhe!"),
+                      content: Text(
+                          "Tämä sivu on vain Järjestelmävalvojille!"),
+                      actions: [
+                        TextButton(
+                          child: Text("OK"),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              }
+            },
           ),
         ],
-      );
-    },
-  );
-  };
-}),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 100,
+      ),
+body: SingleChildScrollView(
+  child: Column(
+    children: [
+      SizedBox(
+        height: 100,
+      ),
+      Container(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Etsi...',
+            prefixIcon: Icon(Icons.search),
+            border: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.grey,
               ),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Etsi...',
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey,
-                      ),
-                      borderRadius: BorderRadius.circular(40.0),
-                    ),
+              borderRadius: BorderRadius.circular(40.0),
+            ),
+          ),
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.only(top: 16),
+        child: Column(
+          children: [
+            Container(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Kuumat uutuudet',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 25,
                   ),
                 ),
               ),
+            ),
+          ],
+        ),
+      ),
+      SizedBox(
+        height: 150,
+        child: GridView.count(
+          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+          scrollDirection: Axis.horizontal,
+          crossAxisCount: 1,
+          shrinkWrap: true,
+          childAspectRatio: (150 / 195),
+          children: [
+            for (int i = 0; i < 5; i++)
               Container(
-                padding: EdgeInsets.only(top: 16),
-                child: Column(
-                  children: [
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 13),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color(0xFF212325),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.4),
+                      spreadRadius: 1,
+                      blurRadius: 8,
+                    )
+                  ]
+                ),
+              ),
+          ],
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.only(top: 16),
+        child: Column(
+          children: [
+            Container(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Näitä luetaan juuri nyt',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 150,
+              child: GridView.count(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                scrollDirection: Axis.horizontal,
+                crossAxisCount: 1,
+                shrinkWrap: true,
+                childAspectRatio: (150 / 195),
+                children: [
+                  for (int i = 0; i < 5; i++)
                     Container(
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Kuumat uutuudet',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 25,
-                          ),
+                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 13),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xFF212325),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.4),
+                            spreadRadius: 1,
+                            blurRadius: 8,
+                          )
+                        ]
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 16),
+              child: Column(
+                children: [
+                  Container(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Koska luit xyz',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 25,
                         ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 200,
-                child: GridView.count(
-                  scrollDirection: Axis.horizontal,
-                  crossAxisCount: 1,
-                  padding: EdgeInsets.only(top: 8),
-                  childAspectRatio: 1.5,
-                  children: List.generate(100, (index) {
-                    return Center(
-                      child: Text(
-                        'Item $index',
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                    );
-                  }),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 16),
-                child: Column(
-                  children: [
+            ),
+            SizedBox(
+              height: 150,
+              child: GridView.count(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                scrollDirection: Axis.horizontal,
+                crossAxisCount: 1,
+                shrinkWrap: true,
+                childAspectRatio: (150 / 195),
+                children: [
+                  for (int i = 0; i < 5; i++)
                     Container(
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Näitä luetaan juuri nyt',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 25,
-                          ),
-                        ),
+                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 13),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color(0xFF212325),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.4),
+                            spreadRadius: 1,
+                            blurRadius: 8,
+                          )
+                        ]
                       ),
                     ),
-              SizedBox(
-                height: 200,
-                child: GridView.count(
-                  scrollDirection: Axis.horizontal,
-                  crossAxisCount: 1,
-                  padding: EdgeInsets.only(top: 8),
-                  childAspectRatio: 1.5,
-                  children: List.generate(100, (index) {
-                    return Center(
-                      child: Text(
-                        'Item $index',
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                    );
-                  }),
-                ),
+                ],
               ),
-              Container(
-                padding: EdgeInsets.only(top: 16),
-                child: Column(
-                  children: [
-                    Container(
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Koska luit xyz',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 25,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 200,
-                child: GridView.count(
-                  scrollDirection: Axis.horizontal,
-                  crossAxisCount: 1,
-                  padding: EdgeInsets.only(top: 8),
-                  childAspectRatio: 1.5,
-                  children: List.generate(100, (index) {
-                    return Center(
-                      child: Text(
-                        'Item $index',
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                    );
-                  }),
-                ),
-              ),
+            ),
             ],
           ),
         ),
