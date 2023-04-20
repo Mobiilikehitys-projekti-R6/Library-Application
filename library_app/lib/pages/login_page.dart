83f5314e68f9dedf1b8ca5001ca04a5c24b00077
temonn <t1poti00@students.oamk.fi>
2023-04-20 12:19:25 +0300

    Reset.dart UI parannettu

--- library_app/lib/pages/login_page.dart
+++ library_app/lib/pages/login_page.dart
@@ -225,7 +225,11 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextButton(
-                  child: Text('Forgot Password?'),
+                  child: Text('Unohtuiko salasana?',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF25BE70),
+                          ),),
                   onPressed: () => Navigator.of(context).push(
                         MaterialPageRoute(builder: (context) => ResetScreen()),
                       ))
--- library_app/lib/pages/reset.dart
+++ library_app/lib/pages/reset.dart
@@ -13,41 +13,76 @@ class _ResetScreenState extends State<ResetScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  
-      appBar: AppBar(title: Text('Reset Password'),),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(  
-                hintText: 'Email'
-              ),
-               onChanged: (value) {
-                setState(() {
-                  email = value.trim();
-                });
-              },
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+            child: Column(children: [
+          const SizedBox(height: 50),
+
+          // Hello!
+          const Text(
+            'Nollaa salasana',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              ElevatedButton(
-                child: Text('Send Request'),
+
+          const SizedBox(height: 70),
+
+          // email textfield
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Sähköposti',
+                  ),
+                    onChanged: (value) {
+                      setState(() {
+                        email = value.trim();               
+                });
+  }),
+              ),
+            ),
+          ),
+          const SizedBox(height: 25),
+          ElevatedButton(
+            child: Text(
+              'Lähetä pyyntö',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0xFF25BE70),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              textStyle: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
+              padding: EdgeInsets.symmetric(vertical: 15, horizontal: 14),
+            ),
                 onPressed: () {
                   auth.sendPasswordResetEmail(email: email);
                   Navigator.of(context).pop();
                 },
-                
-                )
-          
-
-            ],
           ),
-
-        ],),
+        ])),
+      ),
     );
   }
 }
