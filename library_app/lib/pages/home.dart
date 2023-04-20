903be8ef1e67f955a9114e45762d60e9272d8b6f
temonn <t1poti00@students.oamk.fi>
2023-04-20 18:38:43 +0300

    Nyt näkyy kirjojen nimet

--- library_app/lib/pages/home.dart
+++ library_app/lib/pages/home.dart
@@ -3,60 +3,40 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:library_app/pages/addBooks.dart';
 
+FirebaseFirestore db = FirebaseFirestore.instance;
 
-Future<List<List<Map<String, dynamic>>>> getDataFromFolders() async {
-  try {
-    final FirebaseStorage storage = FirebaseStorage.instance;
-    final ListResult result = await storage.ref().child('book-images').listAll();
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
 
-    final List<List<Map<String, dynamic>>> dataArrays = [];
-
-    for (final Reference ref in result.items) {
-      final List<Map<String, dynamic>> dataArray = [];
-
-      final ListResult filesResult = await ref.listAll();
-
-      for (final Reference fileRef in filesResult.items) {
-        final String imageUrl = await fileRef.getDownloadURL();
-
-        if (imageUrl.isEmpty) {
-          print('Download URL is empty');
-        }
-
-        final QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-            .collection('books')
-            .where('image_url', isEqualTo: imageUrl)
-            .get();
-
-        if (querySnapshot.docs.isEmpty) {
-          print('No documents found with the image_url: $imageUrl');
-        }
-
-        final Map<String, dynamic> data = {
-          'name': querySnapshot.docs.first['name'],
-          'image_url': imageUrl,
-        };
-
-        dataArray.add(data);
-      }
-
-      dataArrays.add(dataArray);
-    }
-
-    return dataArrays;
-  } catch (e) {
-    print('Error: $e');
-    rethrow;
-  }
+  @override
+  _MyHomeState createState() => _MyHomeState();
 }
 
+class _MyHomeState extends State<MyHome> {
+  List<Map<String, dynamic>> bookList = [];
 
+  @override
+  void initState() {
+    super.initState();
+    getDataOnce_getAllDocumentsInACollection();
+  }
 
+  void getDataOnce_getAllDocumentsInACollection() {
+    db.collection("books").get().then(
+          (querySnapshot) {
+            print("Data retrieved!");
+            List<Map<String, dynamic>> data = [];
+            for (var docSnapshot in querySnapshot.docs) {
+              data.add(docSnapshot.data());
+            }
+            setState(() {
+              bookList = data;
+            });
+          },
+          onError: (e) => print("Error completing: $e"),
+    );
+  }
 
-
-
-class MyHome extends StatelessWidget {
-  MyHome({Key? key});
   @override
   Widget build(BuildContext context) => Scaffold(
       extendBodyBehindAppBar: true,
@@ -114,10 +94,10 @@ class MyHome extends StatelessWidget {
 body: SingleChildScrollView(
   child: Column(
     children: [
-      SizedBox(
+      SizedBox(      
         height: 100,
       ),
-      Container(
+      Container(       
         padding: const EdgeInsets.all(8.0),
         child: TextField(
           decoration: InputDecoration(
@@ -132,78 +112,65 @@ body: SingleChildScrollView(
           ),
         ),
       ),
-Container(
-  padding: EdgeInsets.only(top: 16),
-  child: Column(
-    children: [
       Container(
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            'Kuumat uutuudet',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 25,
+        padding: EdgeInsets.only(top: 16),
+        child: Column(
+          children: [
+            Container(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Uutuudet',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 25,
+                  ),
+                ),
+                ),),
+               ]         
+              ),
+            ),
+          SizedBox(
+            height: 180,
+            child: GridView.count(
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              scrollDirection: Axis.horizontal,
+              crossAxisCount: 1,
+              shrinkWrap: true,
+              childAspectRatio: (150 / 195),
+              children: [
+                for (var bookData in bookList)
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 13),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xFF212325),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          spreadRadius: 1,
+                          blurRadius: 8,
+                        )
+                      ]
+                    ),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  bookData["name"],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15,
+                  ),
+                ),
+                ),
+                  ),
+              ],
             ),
           ),
-        ),
-      ),
-    ],
-  ),
-),
-FutureBuilder<List<List<Map<String, dynamic>>>>(
-  future: getDataFromFolders(),
-  builder: (context, snapshot) {
-    if (snapshot.connectionState == ConnectionState.waiting) {
-      return CircularProgressIndicator();
-    }
-
-    if (snapshot.hasError) {
-      return Text('Error: ${snapshot.error}');
-    } else {
-      print('We have data! ${snapshot.data}');
-    }
-
-    final dataArrays = snapshot.data!;
-
-    return SizedBox(
-      height: 150,
-      child: GridView.count(
-        padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-        scrollDirection: Axis.horizontal,
-        crossAxisCount: 1,
-        shrinkWrap: true,
-        childAspectRatio: (150 / 195),
-        children: [
-          for (final dataArray in dataArrays)
-            for (int i = 0; i < dataArray.length; i++)
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.network(
-                      dataArray[i]['image_url'],
-                      height: 120,
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                      dataArray[i]['name'],
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-        ],
-      ),
-    );
-  },
-),
-
-
       Container(
         padding: EdgeInsets.only(top: 16),
         child: Column(
@@ -222,7 +189,7 @@ FutureBuilder<List<List<Map<String, dynamic>>>>(
               ),
             ),
             SizedBox(
-              height: 150,
+            height: 180,
               child: GridView.count(
                 padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
                 scrollDirection: Axis.horizontal,
@@ -270,7 +237,7 @@ FutureBuilder<List<List<Map<String, dynamic>>>>(
               ),
             ),
             SizedBox(
-              height: 150,
+            height: 180,
               child: GridView.count(
                 padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
                 scrollDirection: Axis.horizontal,
