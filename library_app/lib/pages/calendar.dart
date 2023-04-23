92547a2eb8c8a930531f08ff02040f6da91e1665
temonn <t1poti00@students.oamk.fi>
2023-04-23 16:12:44 +0300

    Kalenteri näyttää eräpäivät

--- library_app/lib/pages/calendar.dart
+++ library_app/lib/pages/calendar.dart
@@ -28,14 +28,21 @@ class _MyCalendarState extends State<MyCalendar> {
     db.collection("users").doc(user?.uid).get().then((docSnapshot) async {
       List<dynamic> loanedBooks = docSnapshot.data()?["loanedBooks"];
 
+      Map<DateTime, List<Event>> events = {};
+
       loanedBooks.forEach((loanedBook) {
         DateTime dueDate = loanedBook["returnDate"].toDate();
 
-        if (_events[dueDate] == null) {
-          _events[dueDate] = [];
+        if (events[dueDate] == null) {
+          events[dueDate] = [];
         }
-        _events[dueDate]!.add(Event(loanedBook["bookName"]));
+        events[dueDate]!.add(Event(loanedBook["bookName"]));
       });
+
+      setState(() {
+        _events = events;
+      });
+
       print(_events);
     }).catchError((e) => print("Error completing: $e"));
   }
@@ -46,127 +53,129 @@ class _MyCalendarState extends State<MyCalendar> {
     _retrieveEvents();
   }
 
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Color.fromARGB(68, 255, 255, 255),
-      elevation: 0,
-      title: const Text(
-        'Kalenteri',
-        style: TextStyle(
-          color: Colors.black,
-          fontWeight: FontWeight.bold,
-          fontSize: 27,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(68, 255, 255, 255),
+        elevation: 0,
+        title: const Text(
+          'Kalenteri',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 27,
+          ),
         ),
+        centerTitle: true,
       ),
-      centerTitle: true,
-    ),
-    body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TableCalendar(
-          calendarFormat: _calendarFormat,
-          focusedDay: _focusedDay,
-          firstDay: DateTime(2010),
-          lastDay: DateTime(2050),
-          startingDayOfWeek: StartingDayOfWeek.monday,
-          calendarBuilders: CalendarBuilders(
-            markerBuilder: (context, day, events) {
-              final isSelectedDay = isSameDay(_selectedDay, day);
-              final isEventDay = _events.keys.contains(day);
-              final color = isSelectedDay
-                  ? Colors.blue
-                  : isEventDay
-                  ? Colors.red
-                  : Colors.transparent;
-              return Container(
-                width: 4,
-                height: 4,
-                margin: const EdgeInsets.symmetric(vertical: 2),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: color,
-                ),
-              );
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TableCalendar(
+            calendarFormat: _calendarFormat,
+            focusedDay: _focusedDay,
+            firstDay: DateTime(2010),
+            lastDay: DateTime(2050),
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            calendarBuilders: CalendarBuilders(
+              markerBuilder: (context, day, events) {
+                final isSelectedDay = isSameDay(_selectedDay, day);
+                final isEventDay = _events.keys.contains(day);
+                final color = isSelectedDay
+                    ? Colors.blue
+                    : isEventDay
+                        ? Colors.red
+                        : Colors.transparent;
+                return Container(
+                  width: 4,
+                  height: 4,
+                  margin: const EdgeInsets.symmetric(vertical: 2),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: color,
+                  ),
+                );
+              },
+            ),
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            },
+            onFormatChanged: (format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+            eventLoader: (day) {
+              if (_events[day] != null) {
+                return _events[day]!
+                    .map((event) => ListTile(
+                          title: Text(event.bookTitle),
+                        ))
+                    .toList();
+              }
+              return [];
             },
           ),
-          selectedDayPredicate: (day) {
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-            });
-          },
-          onFormatChanged: (format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          },
-          onPageChanged: (focusedDay) {
-            _focusedDay = focusedDay;
-          },
-          eventLoader: (day) {
-            if (_events[day] != null) {
-              return _events[day]!.map((event) => event.bookTitle).toList();
-            }
-            return [];
-          },
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            "Upcoming Events",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Upcoming Events",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
             ),
           ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: _events.length,
-            itemBuilder: (context, index) {
-              final eventDate = _events.keys.elementAt(index);
-              final events = _events[eventDate]!;
-              final formattedDate =
-                  DateFormat.yMMMMd().format(eventDate);
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    child: Text(
-                      formattedDate,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  for (final event in events)
+          Expanded(
+            child: ListView.builder(
+              itemCount: _events.length,
+              itemBuilder: (context, index) {
+                final eventDate = _events.keys.elementAt(index);
+                final events = _events[eventDate]!;
+                final formattedDate = DateFormat.yMMMMd().format(eventDate);
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     Padding(
                       padding: const EdgeInsets.symmetric(
                         horizontal: 16,
-                        vertical: 4,
+                        vertical: 8,
                       ),
-                      child: ListTile(
-                        title: Text(event.bookTitle),
+                      child: Text(
+                        formattedDate,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
-                ],
-              );
-            },
+                    for (final event in events)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 4,
+                        ),
+                        child: ListTile(
+                          title: Text(event.bookTitle),
+                        ),
+                      ),
+                  ],
+                );
+              },
+            ),
           ),
-        ),
-      ],
-    ),
-  );
-}
+        ],
+      ),
+    );
+  }
 }
